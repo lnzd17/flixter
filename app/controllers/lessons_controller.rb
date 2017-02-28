67dfdf6d85cd9a052fb0b77902047825eeb59c67
Lindsay Dages <lindsaykayedutton@gmail.com>
2017-02-28 19:50:49 +0000

    restricted access to lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,9 +1,19 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson
+
+
   def show
   end
 
 
   private
+
+  def require_authorized_for_current_lesson
+    unless current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path(current_lesson.section.course), alert:'Not Authorized...Please enroll first'
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
