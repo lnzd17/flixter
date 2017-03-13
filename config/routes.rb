30963ae54e22fb81be950e6da277d676da50846b
Lindsay Dages <lindsaykayedutton@gmail.com>
2017-03-13 14:15:00 +0000

    Added more info to footer

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,24 @@ Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#index'
   resource :dashboard, only: [:show]
+
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
   end
+
   resources :lessons, only: [:show]
+
   namespace :instructor do
     resources :lessons, only: [:update]
+
     resources :sections, only: [:update] do
       resources :lessons, only: [:create]
     end
+
     resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:create]
     end
+
   end
+
 end
